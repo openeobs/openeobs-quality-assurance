d5679ff5a481870268611904038c6b9488b54cc7
amipatel <amipatel@neovahealth.co.uk>
2016-01-22 12:14:34 +0000

    adding test for Senior Manager persmissions

--- tests/test_SM_mobile_login.py
+++ tests/test_SM_mobile_login.py
@@ -1,4 +1,3 @@
-__author__ = 'amipatel'
 from test_common import TestCommon
 from openeobs_selenium.login_page import LoginPage
 from openeobs_selenium.task_page import TaskPage
@@ -10,6 +9,7 @@ class TestSMPatientPage(TestCommon):
         self.driver.get("http://localhost:8069/mobile/")
         self.login_page = LoginPage(self.driver)
         self.task_list_page = ListPage(self.driver)
+        self.patient_list_page = ListPage(self.driver)
         self.login_page.login('saint', 'saint')
         self.task_list_page.go_to_task_list()
 
@@ -19,13 +19,23 @@ class TestSMPatientPage(TestCommon):
 
     def test_has_no_task(self):
         """
-        Test That Task list show no task for user
+        Test that task list show no task for user
         """
         task_list=[]
 
         for patient in self.task_list_page.get_list_items():
             task_list.append(patient)
+            self.assertEquals(task_list,[],'There are no tasks')
 
-        self.assertEquals(task_list,[],'There are no tasks')
+    def test_can_not_take_patient_observation(self):
+        """
+        Test that user can not take observation
+        """
+        patients = self.patient_list_page.get_list_items()
+        patients_to_test = patients[0]
+        patients_to_test.click()
+
+        self.
+
 
 
