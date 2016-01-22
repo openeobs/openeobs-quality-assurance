b322b8bd0b40d3bf1bbf75a3f4d75b349887e6aa
amipatel <amipatel@neovahealth.co.uk>
2016-01-22 10:14:24 +0000

    adding test Senior Manager can see patient list when logged in on mobile version of eobs

--- tests/test_SM_mobile_login.py
+++ tests/test_SM_mobile_login.py
@@ -2,20 +2,30 @@ __author__ = 'amipatel'
 from test_common import TestCommon
 from openeobs_selenium.login_page import LoginPage
 from openeobs_selenium.task_page import TaskPage
-from openeobs_selenium.patient_page import PatientPage
-class TestSMPatientPage(TestCommon):
+from openeobs_selenium.list_page import ListPage
 
+
+class TestSMPatientPage(TestCommon):
     def setUp(self):
-        self.driver.get("http://localhost:8069/mobile/patients/")
+        self.driver.get("http://localhost:8069/mobile/")
         self.login_page = LoginPage(self.driver)
-        self.task_list_page = TaskPage(self.driver)
-        self.login_page.login('saint','saint')
+        self.task_list_page = ListPage(self.driver)
+        self.login_page.login('saint', 'saint')
         self.task_list_page.go_to_task_list()
 
     def test_can_logout(self):
         self.task_list_page.logout()
         self.assertTrue(self.task_list_page.is_login_page(), 'did notget to the logout')
-    #
-    # def test_can_see_no_patient(self):
-    #     patient_list = PatientPage.has_patient()
-    #     print list(patient_list)
+
+    def test_has_no_task(self):
+        """
+        Test That Task list show no task for user
+        """
+        task_list=[]
+
+        for patient in self.task_list_page.get_list_items():
+            task_list.append(patient)
+
+        self.assertEquals(task_list,[],'There are no tasks')
+
+
