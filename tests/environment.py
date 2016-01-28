07fe0e4ae285b8c4023d99dde8ea8dca26af43b7
amipatel <amipatel@neovahealth.co.uk>
2016-01-28 11:50:37 +0000

    Adding environment file, parametrized config variables

---
+++ tests/environment.py
@@ -0,0 +1,6 @@
+class configVars:
+
+        database = '2701'
+        url = 'http://localhost:8069/web?db=' + database
+        odoo_client_url = 'http://localhost:8069'
+        test_db_name = 'openeobs_quality_assurance_db'
--- tests/test_common.py
+++ tests/test_common.py
@@ -4,28 +4,37 @@ import selenium.webdriver.support.expected_conditions as ec
 import selenium.webdriver.support.ui as ui
 from selenium.webdriver.common.by import By
 from erppeek import Client
+from environment import configVars
 
 
 class TestCommon(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        database = configVars.database
+        url = configVars.url
+        odoo_client_url = configVars.odoo_client_url
+        test_db = configVars.test_db_name
+
         cls.driver = webdriver.Firefox()
-        cls.driver.get('http://localhost:8069/web?db=mobile_qa_db')
+        cls.driver.get(url)
         ui.WebDriverWait(cls.driver, 5).until(
             ec.visibility_of_element_located(
                     (By.CSS_SELECTOR,
                      '.oe_single_form_container.modal-content')
             )
         )
-        cls.odoo_client = Client('http://localhost:8069', db='mobile_qa_db',
+        cls.odoo_client = Client(odoo_client_url, db=database,
                                  user='admin', password='admin')
-        cls.test_database_name = 'openeobs_quality_assurance_db'
+
+        cls.test_database_name = test_db
+
         cls.odoo_client.db.drop('admin', cls.test_database_name)
-        cls.odoo_client.db.duplicate_database('admin', 'mobile_qa_db',
+        cls.odoo_client.db.duplicate_database('admin', database ,
                                               cls.test_database_name)
 
     @classmethod
     def tearDownClass(cls):
         cls.odoo_client.db.drop('admin', cls.test_database_name)
         cls.driver.close()
+
