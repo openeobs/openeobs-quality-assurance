0834f4955846be7b101590408a6b5264349d4bac
David Teece <david@neovahealth.co.uk>
2016-04-21 16:58:20 +0100

    Added function for getting desktop URL

--- openeobs_selenium/environment.py
+++ openeobs_selenium/environment.py
@@ -5,13 +5,15 @@ USERNAME = 'admin'
 PASSWORD = 'admin'
 # DB names
 TEST_DB_NAME = 'openeobs_quality_assurance_db'
-DATABASE = 'nhclinical'
+DATABASE = 'slam'
 # Url Info
 ODOO_CLIENT_URL = 'http://localhost:8069'
 # DESKTOP_URL = 'http://localhost:8069/web?db={database}'.format(
 #     database=DATABASE)
 DESKTOP_URL = ODOO_CLIENT_URL+'/web?db={database}'.format(database=DATABASE)
 
+def get_desktop_url():
+    return ODOO_CLIENT_URL+'/web?db={database}'.format(database=DATABASE)
 # MOB_LOGIN = 'http://localhost:8069/mobile/login'
 MOB_LOGIN = ODOO_CLIENT_URL + '/mobile/login'
 PATIENT_PAGE = ODOO_CLIENT_URL + '/mobile/patient/'
