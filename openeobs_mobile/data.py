e0f14cb76082252ec836b5b2d365d8a15cf25b90
David Teece <david@neovahealth.co.uk>
2016-04-25 17:06:49 +0100

    Changed dictionaries for MEDIUM_RISK data into lists of dictionaries.

--- openeobs_mobile/data.py
+++ openeobs_mobile/data.py
@@ -197,54 +197,169 @@ LOW_RISK_SCORE_4_EWS_DATA = [
 
 LOW_SCORE_RESPONSE = 'Assess Patient'
 
-MEDIUM_RISK_SCORE_3_THREE_IN_ONE_EWS_DATA = {
-    'respiration_rate': 18,
-    'indirect_oxymetry_spo2': 99,
-    'oxygen_administration_flag': 'No',
-    'blood_pressure_systolic': 120,
-    'blood_pressure_diastolic': 80,
-    'avpu_text': 'Voice',
-    'pulse_rate': 65,
-    'body_temperature': 37.5
-}
+MEDIUM_RISK_SCORE_3_THREE_IN_ONE_EWS_DATA = [
+    {'name': 'respiration_rate',
+     'value': 18,
+     'type': 'textbox'
+     },
 
-MEDIUM_RISK_SCORE_4_THREE_IN_ONE_EWS_DATA = {
-    'respiration_rate': 11,
-    'indirect_oxymetry_spo2': 99,
-    'oxygen_administration_flag': 'No',
-    'blood_pressure_systolic': 120,
-    'blood_pressure_diastolic': 80,
-    'avpu_text': 'Voice',
-    'pulse_rate': 65,
-    'body_temperature': 37.5
-}
+    {'name': 'indirect_oxymetry_spo2',
+     'value': 99,
+     'type': 'textbox'
+     },
+    {'name': 'oxygen_administration_flag',
+     'value': 'No',
+     'type': 'select'},
 
-MEDIUM_RISK_SCORE_5_EWS_DATA = {
-    'respiration_rate': 11,
-    'indirect_oxymetry_spo2': 99,
-    'oxygen_administration_flag': 'Yes',
-    'blood_pressure_systolic': 110,
-    'blood_pressure_diastolic': 80,
-    'avpu_text': 'Alert',
-    'pulse_rate': 50,
-    'body_temperature': 37.5,
-    'device_id': 'Nasal Cannula',
-    'concentration': 40
-}
+    {'name': 'blood_pressure_systolic',
+     'value': 120,
+     'type': 'textbox'},
 
-MEDIUM_RISK_SCORE_6_EWS_DATA = {
-    'respiration_rate': 24,
-    'indirect_oxymetry_spo2': 99,
-    'oxygen_administration_flag': 'Yes',
-    'blood_pressure_systolic': 110,
-    'blood_pressure_diastolic': 80,
-    'avpu_text': 'Alert',
-    'pulse_rate': 50,
-    'body_temperature': 37.5,
-    'device_id': 'CPAP',
-    'concentration': 60,
-    'cpap_peep': 2
-}
+    {'name': 'blood_pressure_diastolic',
+     'value': 80,
+     'type': 'textbox'},
+
+    {'name': 'avpu_text',
+     'value': 'Voice',
+     'type': 'select'},
+
+    {'name': 'pulse_rate',
+     'value': 65,
+     'type': 'textbox'},
+
+    {'name': 'body_temperature',
+     'value': 37.5,
+     'type': 'textbox'}
+
+]
+
+MEDIUM_RISK_SCORE_4_THREE_IN_ONE_EWS_DATA = [
+    {'name': 'respiration_rate',
+     'value': 11,
+     'type': 'textbox'
+     },
+
+    {'name': 'indirect_oxymetry_spo2',
+     'value': 99,
+     'type': 'textbox'
+     },
+    {'name': 'oxygen_administration_flag',
+     'value': 'No',
+     'type': 'select'},
+
+    {'name': 'blood_pressure_systolic',
+     'value': 120,
+     'type': 'textbox'},
+
+    {'name': 'blood_pressure_diastolic',
+     'value': 80,
+     'type': 'textbox'},
+
+    {'name': 'avpu_text',
+     'value': 'Voice',
+     'type': 'select'},
+
+    {'name': 'pulse_rate',
+     'value': 65,
+     'type': 'textbox'},
+
+    {'name': 'body_temperature',
+     'value': 37.5,
+     'type': 'textbox'}
+
+]
+
+MEDIUM_RISK_SCORE_5_EWS_DATA = [
+    {'name': 'respiration_rate',
+     'value': 11,
+     'type': 'textbox'
+     },
+
+    {'name': 'indirect_oxymetry_spo2',
+     'value': 99,
+     'type': 'textbox'
+     },
+    {'name': 'oxygen_administration_flag',
+     'value': 'Yes',
+     'type': 'select'},
+
+    {'name': 'blood_pressure_systolic',
+     'value': 110,
+     'type': 'textbox'},
+
+    {'name': 'blood_pressure_diastolic',
+     'value': 80,
+     'type': 'textbox'},
+
+    {'name': 'avpu_text',
+     'value': 'Alert',
+     'type': 'select'},
+
+    {'name': 'pulse_rate',
+     'value': 50,
+     'type': 'textbox'},
+
+    {'name': 'body_temperature',
+     'value': 37.5,
+     'type': 'textbox'},
+
+    {'name': 'device_id',
+     'value': 'Nasal Cannula',
+     'type': 'select'},
+
+    {'name': 'concentration',
+     'value': 40,
+     'type': 'textbox'},
+
+]
+
+MEDIUM_RISK_SCORE_6_EWS_DATA = [
+    {'name': 'respiration_rate',
+     'value': 24,
+     'type': 'textbox'
+     },
+
+    {'name': 'indirect_oxymetry_spo2',
+     'value': 99,
+     'type': 'textbox'
+     },
+    {'name': 'oxygen_administration_flag',
+     'value': 'Yes',
+     'type': 'select'},
+
+    {'name': 'blood_pressure_systolic',
+     'value': 110,
+     'type': 'textbox'},
+
+    {'name': 'blood_pressure_diastolic',
+     'value': 80,
+     'type': 'textbox'},
+
+    {'name': 'avpu_text',
+     'value': 'Alert',
+     'type': 'select'},
+
+    {'name': 'pulse_rate',
+     'value': 50,
+     'type': 'textbox'},
+
+    {'name': 'body_temperature',
+     'value': 37.5,
+     'type': 'textbox'},
+
+    {'name': 'device_id',
+     'value': 'CPAP',
+     'type': 'select'},
+
+    {'name': 'concentration',
+     'value': 60,
+     'type': 'textbox'},
+
+    {'name': 'cpap_peep',
+     'value': 2,
+     'type': 'textbox'},
+
+]
 
 MEDIUM_SCORE_RESPONSE = 'Urgently inform medical team'
 
