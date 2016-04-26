7b1db96025ac9022dd36392da31188dc74babf72
David Teece <david@neovahealth.co.uk>
2016-04-26 15:05:33 +0100

    Added selector for finding desktop login button.

--- openeobs_mobile/login_page_locators.py
+++ openeobs_mobile/login_page_locators.py
@@ -4,5 +4,6 @@ from selenium.webdriver.common.by import By
 USERNAME_EL = (By.ID, 'username')
 PASSWORD_EL = (By.ID, 'password')
 LOGIN_BUTTON_EL = (By.ID, 'loginbutton')
+LOGIN_BUTTON_DESKTOP = (By.CLASS_NAME, 'btn btn-primary')
 DATABASE_DROPDOWN_EL = (By.ID, 'database')
 ERROR_EL = (By.CSS_SELECTOR, '.alert.alert-error')
