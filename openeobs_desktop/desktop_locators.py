1204195a5f42be0fe27d36406413f862baedb73e
David Teece <david@neovahealth.co.uk>
2016-04-27 11:07:53 +0100

    Added directory for locators related to desktop, and locator file.

---
+++ openeobs_desktop/desktop_locators.py
@@ -0,0 +1,14 @@
+"""Helpers for the desktop interface"""
+
+from selenium.webdriver.common.by import By
+
+DESKTOP_TITLE = (By.CLASS_NAME, 'oe_view_title')
+KANBAN_BOARD = (By.CLASS_NAME, 'oe_view_manager_wrapper')
+KANBAN_ITEMS = (By.CLASS_NAME, 'oe_kanban_content')
+PATIENT_RECORD = (By.CLASS_NAME, 'oe_form_sheetbg')
+ADMISSION_DETAILS = (By.ID, 'ui-id-1')
+DEMOGRAPHICS = (By.ID, 'ui-id-2')
+OTHER_OBS = (By.ID, 'ui-id-3')
+DEVICES = (By.ID, 'ui-id-4')
+MONITORING = (By.ID, 'ui-id-5')
+
