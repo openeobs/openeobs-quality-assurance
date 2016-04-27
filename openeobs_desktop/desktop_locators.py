35f284ee5fbd546eec53dde862e703d414dbf8fb
David Teece <david@neovahealth.co.uk>
2016-04-27 11:50:06 +0100

    Added locator for patient height.

--- openeobs_desktop/desktop_locators.py
+++ openeobs_desktop/desktop_locators.py
@@ -11,4 +11,4 @@ DEMOGRAPHICS = (By.ID, 'ui-id-2')
 OTHER_OBS = (By.ID, 'ui-id-3')
 DEVICES = (By.ID, 'ui-id-4')
 MONITORING = (By.ID, 'ui-id-5')
-
+HEIGHT = (By.CSS_SELECTOR, '.oe_form_field_float.nhc_selenium_height')
